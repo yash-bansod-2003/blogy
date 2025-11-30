633638f05e5f5f899eae90828a0f72198f42ee5c
yashbansod <yashbansodgit@gmail.com>
2025-11-30 23:03:57 +0530

    feat: add Blog model and update Category options

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -13,3 +14,26 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+STATUS_CHOICES = (
+    ("draft", "Draft"),
+    ("published", "Published"),
+)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="blogs")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to="blog_images/")
+    description = models.TextField(max_length=500)
+    content = models.TextField()
+    status = models.CharField(choices=STATUS_CHOICES, default="draft")
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
