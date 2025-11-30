036d1e8928407acbdbcf8ba3b7ef85e4baeb52a3
yashbansod <yashbansodgit@gmail.com>
2025-12-01 00:07:31 +0530

    feat: update home page to display blogs with titles, descriptions, and images

--- blogy/views.py
+++ blogy/views.py
@@ -8,8 +8,10 @@ def home(request):
     featured_blogs = Blog.objects.filter(is_featured=True, status="published").order_by(
         "-created_at"
     )[:5]
+    blogs = Blog.objects.filter(status="published").order_by("-created_at")[:10]
     context = {
         "categories": categories,
         "featured_blogs": featured_blogs,
+        "blogs": blogs,
     }
     return render(request, "home.html", context)
