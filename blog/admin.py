93947a0c103e53141a33055eb06020529b0f3770
yashbansod <yashbansodgit@gmail.com>
2025-11-30 23:12:42 +0530

    feat: enhance Blog admin interface with custom display and filtering options

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from .models import Blog, Category
 
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "status", "is_featured", "created_at")
+    list_filter = ("status", "is_featured", "category")
+    list_editable = ("status", "is_featured")
+    search_fields = ("title", "description", "content")
+    # if we want to search by related model fields so we use double underscore
+    # search_fields = ("category__name")
+    prepopulated_fields = {"slug": ("title",)}
+    date_hierarchy = "created_at"
+    ordering = ("-created_at",)
+
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
