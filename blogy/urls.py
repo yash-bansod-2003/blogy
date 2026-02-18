333979e0ad08220ebc26a42b4190eaffcc30fddd
Yash Bansod <yashbansodgit@gmail.com>
2026-02-18 17:33:49 +0530

    feat: implement user registration form and update navigation for registration

--- blogy/urls.py
+++ blogy/urls.py
@@ -20,10 +20,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from blogy.views import home
+from blogy.views import home, register
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
+    path("auth/register/", register, name="register"),
     path("blog/", include("blog.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogy/views.py
+++ blogy/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from blog.models import Blog, Category
-
+from blogy.forms import RegisterForm
 
 def home(request):
     categories = Category.objects.all()
@@ -15,3 +15,15 @@ def home(request):
         "blogs": blogs,
     }
     return render(request, "home.html", context)
+
+
+def register(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "home.html")
+    else:
+        form = RegisterForm()
+        return render(request, "auth/register.html", {"form": form})
