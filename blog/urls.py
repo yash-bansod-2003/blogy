0d8669630d0e03626195aa68f3445ffd831f2bb1
Yash Bansod <yashbansodgit@gmail.com>
2026-02-18 16:55:11 +0530

    feat: add blogs by category view and template

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import blog_detail
+from .views import blog_detail, blogs_by_category
 
 urlpatterns = [
     # Blog app URL patterns will go here
     path("<slug:slug>", blog_detail, name="blog_detail"),
+    path("category/<int:category_id>/", blogs_by_category, name="blogs_by_category"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,14 @@ def blog_detail(request, slug):
         "blog": blog,
     }
     return render(request, "blog/blog_detail.html", context)
+
+def blogs_by_category(request, category_id):
+    categories = Category.objects.all()
+    category = get_object_or_404(Category, id=category_id)
+    blogs = Blog.objects.filter(category=category, status="published")
+    context = {
+        "categories": categories,
+        "category": category,
+        "posts": blogs,
+    }
+    return render(request, "blog/blogs_by_category.html", context)
