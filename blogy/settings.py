8b5e131b1013b0d408ff60cf4b95c8fa4f57324c
Yash Bansod <yashbansodgit@gmail.com>
2026-02-18 16:59:41 +0530

    fix: update DEBUG setting to use environment variable and add ALLOWED_HOSTS feat: add custom 404 error page template

--- blogy/settings.py
+++ blogy/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
 
 
 ALLOWED_HOSTS: list[str] = []
@@ -133,3 +133,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+ALLOWED_HOSTS = ["*"]
