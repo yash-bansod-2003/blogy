a7935932c83ba3979407e9f16e41f15b7c488b48
Yash Bansod <yashbansodgit@gmail.com>
2026-02-18 17:10:28 +0530

    feat: add categories context processor and update templates for category navigation

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,8 @@ from .models import Blog, Category
 
 # Create your views here.
 def blog_detail(request, slug):
-    categories = Category.objects.all()
     blog = get_object_or_404(Blog, slug=slug, status="published")
     context = {
-        "categories": categories,
         "blog": blog,
     }
     return render(request, "blog/blog_detail.html", context)
@@ -18,7 +16,6 @@ def blogs_by_category(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     blogs = Blog.objects.filter(category=category, status="published")
     context = {
-        "categories": categories,
         "category": category,
         "posts": blogs,
     }
